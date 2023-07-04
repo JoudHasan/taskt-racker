7ffdf080d25d88470e1af51b96e75b204ab36141
Joud <joud.hasan85@gmail.com>
2023-07-04 17:09:44 +0200

    new repo

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+let li = $("<li></li>");
+let inputValue = $("#input").val();
+li.text(inputValue);
+
+if (inputValue === "") {
+  alert("You must write something!");
+} else {
+  $("#list").append(li);
+}
+function crossOut() {
+  $(this).toggleClass("strike");
+}
+
+li.on("dblclick", crossOut);
+let crossOutButton = $("<crossOutButton></crossOutButton>").text("X");
+li.append(crossOutButton);
+
+crossOutButton.on("click", function () {
+  li.addClass("delete");
+});
+
+$("#list").sortable();

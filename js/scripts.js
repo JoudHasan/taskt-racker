05f5ebb23ca9a6d2b1a8a33b6af7adb61eed2c6d
Joud <joud.hasan85@gmail.com>
2023-07-05 16:50:45 +0200

    updated link and new item

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,24 @@
-let li = $("<li></li>");
-let inputValue = $("#input").val();
-li.text(inputValue);
+function newItem() {
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.text(inputValue);
 
-if (inputValue === "") {
-  alert("You must write something!");
-} else {
-  $("#list").append(li);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    $("#list").append(li);
+  }
+  function crossOut() {
+    $(this).toggleClass("strike");
+  }
+
+  li.on("dblclick", crossOut);
+  let crossOutButton = $("<crossOutButton></crossOutButton>").text("X");
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", function () {
+    li.addClass("delete");
+  });
+
+  $("#list").sortable();
 }
-function crossOut() {
-  $(this).toggleClass("strike");
-}
-
-li.on("dblclick", crossOut);
-let crossOutButton = $("<crossOutButton></crossOutButton>").text("X");
-li.append(crossOutButton);
-
-crossOutButton.on("click", function () {
-  li.addClass("delete");
-});
-
-$("#list").sortable();
